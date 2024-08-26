d5c8ddcc73014452cc11380bf43bd74c562125c5
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2024-08-26 13:51:22 -0300

    Initial commit

---
+++ src/main/java/deso/delivery_app/Delivery_app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deso.delivery_app;
+
+/**
+ *
+ * @author BMPC
+ */
+public class Delivery_app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
