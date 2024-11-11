d1d38df24d8f902adc3e6181cdc7d7bab1f37b09
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2024-11-11 18:57:23 -0300

    refactor: itemspedido table inside detallepedido view

--- src/main/java/deso/delivery_app/views/pedidos/DetallePedidoIndexForm.java
+++ src/main/java/deso/delivery_app/views/pedidos/DetallePedidoIndexForm.java
@@ -1,14 +1,20 @@
 package deso.delivery_app.views.pedidos;
 
 import deso.delivery_app.ESTADO_PEDIDO;
+import deso.delivery_app.controllers.ItemPedidoController;
 import deso.delivery_app.controllers.PedidoController;
+import deso.delivery_app.models.ItemMenu;
+import deso.delivery_app.models.ItemPedido;
 import deso.delivery_app.models.Pedido;
 import deso.delivery_app.views.AdminLayoutForm;
+import deso.delivery_app.views.components.ButtonColumn;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.List;
 
 public class DetallePedidoIndexForm {
@@ -21,7 +27,10 @@ public class DetallePedidoIndexForm {
     private JButton agregarItemsButton;
     private JLabel titleLabel;
     private JComboBox<String> estadoComboBox;
+    private JPanel rootDetalleTablePanel;
+    private JTable itemsPedidoTable;
     private Pedido pedido;
+    private ItemPedidoController controller;
     private static final String PRICE_FORMAT = "$%.2f";
     private double totalPrice;
 
@@ -31,7 +40,8 @@ public class DetallePedidoIndexForm {
         setLabels(pedido);
         createComboBox(pedido);
         setComboBox(pedido);
-        createDetallePedidoItemsTable(pedido);
+        controller = new ItemPedidoController();
+        createDetallePedidoItemsTable(controller.getLista(pedido));
 
         estadoComboBox.addActionListener(new ActionListener() {
             @Override
@@ -63,13 +73,44 @@ public class DetallePedidoIndexForm {
         });
     }
 
-    public void createDetallePedidoItemsTable(Pedido p) {
-        ItemsPedidoTableForm itemsPedidoTableForm = new ItemsPedidoTableForm(p);
-        itemsPedidoTableForm.onPriceChange(this::updatePrice);
-        DetallePedidoItemsTablePanel.removeAll();
-        DetallePedidoItemsTablePanel.add(itemsPedidoTableForm.getRootPanel(), BorderLayout.CENTER);
-        DetallePedidoItemsTablePanel.revalidate();
-        DetallePedidoItemsTablePanel.repaint();
+    public void createDetallePedidoItemsTable(List<ItemPedido> ims) {
+        String[] columnNames = {"Id", "Nombre", "Precio", "Cantidad", "Total", "Eliminar"};
+        Object[][] data = new Object[ims.size()][columnNames.length];
+        for (int i = 0; i < ims.size(); i++) {
+            ItemPedido ip = ims.get(i);
+            ItemMenu im = ip.getItemMenu();
+            data[i][0] = ip.getId();
+            data[i][1] = im.getNombre();
+            data[i][2] = String.format("$%.2f", im.getPrecio());
+            data[i][3] = ip.getCantidad();
+            data[i][4] = String.format("$%.2f", ip.getPrecioTotal());
+            data[i][5] = "Eliminar";
+        }
+
+        assert itemsPedidoTable != null;
+        itemsPedidoTable.setModel(new DefaultTableModel(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                int eliminarIndex = itemsPedidoTable.getColumnModel().getColumnIndex("Eliminar");
+                //If current row has estado pedido == enviado or preparado, disable delete
+                return column == eliminarIndex && pedido.getEstado() != ESTADO_PEDIDO.ENVIADO && pedido.getEstado() != ESTADO_PEDIDO.PREPARADO;
+            }
+        });
+        Action delete = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTable table = (JTable) e.getSource();
+                int modelRow = Integer.parseInt(e.getActionCommand());
+                long id = (long) table.getModel().getValueAt(modelRow, 0);
+                pedido.removeItem(controller.buscar(id));
+                controller.eliminar(id);
+                ((DefaultTableModel) table.getModel()).removeRow(modelRow);
+                updatePrice();
+            }
+        };
+        int eliminarIndex = itemsPedidoTable.getColumnModel().getColumnIndex("Eliminar");
+        ButtonColumn buttonColumn = new ButtonColumn(itemsPedidoTable, delete, eliminarIndex);
+        buttonColumn.setMnemonic(KeyEvent.VK_D);
     }
 
     public void setLabels(Pedido p) {
