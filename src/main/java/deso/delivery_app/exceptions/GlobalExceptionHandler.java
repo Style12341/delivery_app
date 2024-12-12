c951a3085c49c90d013fe7536fa549158048ad3f
maxi bernard <bmaximiliano0210@gmail.com>
2024-12-12 15:45:22 -0300

    fix: conflict spring doc & exception handler

--- src/main/java/deso/delivery_app/exceptions/GlobalExceptionHandler.java
+++ src/main/java/deso/delivery_app/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package deso.delivery_app.exceptions;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Hidden
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
